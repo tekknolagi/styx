e0b38eb785bb15b3c7324f403596577aca3e7710
Basile Burg <basile.b@gmx.com>
2017-05-23 16:12:38 +0200

    aplication: add option to measure comp dur, fix until default value,    add option to print the ast

--- src/yatol/app.d
+++ src/yatol/app.d
@@ -1,7 +1,7 @@
 module app;
 
 import
-    std.getopt, std.file, std.stdio, std.path;
+    std.getopt, std.file, std.stdio, std.path, std.datetime;
 import
     yatol.lexer, yatol.lexer.types, yatol.parser, yatol.parser.ast,
     yatol.semantic,
@@ -9,6 +9,7 @@ import
 
 enum Until
 {
+    _,
     lexing,
     parsing,
     semantic,
@@ -16,9 +17,47 @@ enum Until
 
 struct Options
 {
-    static bool verbose;
-    static Until until;
+__gshared:
+    bool mtime;
+    bool verbose;
+    bool print;
+    Until until;
 }
+
+alias options = Options;
+
+struct Timer
+{
+__gshared: private:
+
+    ubyte _stage;
+    TickDuration[4] _times;
+    StopWatch _sw;
+
+public:
+
+    static void start()
+    {
+        _sw.start();
+    }
+
+    static void peek()
+    {
+        _times[_stage++] = _sw.peek();
+    }
+
+    static void print()
+    {
+        writeln("duration:");
+        writeln("lexing  : ", dur!"hnsecs"(_times[0].hnsecs));
+        writeln("parsing : ", dur!"hnsecs"((_times[1] - _times[0]).hnsecs));
+        writeln("semantic: ", dur!"hnsecs"((_times[2] - _times[1]).hnsecs));
+        writeln("codegen : ", dur!"hnsecs"((_times[3] - _times[2]).hnsecs));
+        writeln("total   : ", dur!"hnsecs"(_times[3].hnsecs));
+    }
+}
+
+alias timer = Timer;
 
 void showHelp()
 {
@@ -32,9 +71,11 @@ Command line syntax:
 Files:
     a list of yatol source files, space-separated.
 Options:
-    -h --help       : prints this message.
-    -v --verbose    : verbose output.
-    --until=<phase> : compiles and stops after <phase>, either "lexing", "parsing" or "semantic".
+    -h or --help          : prints this message.
+    -p or --print         : print the AST of each source.
+    -t or --time          : measure the time spent to compile.
+          --until=<phase> : compiles and stops after <phase>, either "lexing", "parsing" or "semantic".
+    -v or --verbose       : verbose output.
 `
     );
     stdout.flush;
@@ -42,7 +83,6 @@ Options:
 
 int main(string[] args)
 {
-    Options options;
     GetoptResult gr;
     string[] sources;
     Lexer*[] lexers;
@@ -77,8 +117,11 @@ int main(string[] args)
     }
 
     try gr = getopt(args,
-        "v|verbose", &options.verbose,
+        "p|print", &options.print,
+        "t|time", &options.mtime,
         "until", &options.until,
+        "v|verbose", &options.verbose,
+
     );
     catch (GetOptException ge)
     {
@@ -91,16 +134,34 @@ int main(string[] args)
         return 0;
     }
 
+    if (options.mtime)
+        timer.start();
+
     // lexes each source
     foreach (source; sources)
     {
         if (options.verbose)
             writeln("lexing ", source, "...");
-        lexers ~= new Lexer(source);
-        lexers[$-1].lex();
+        try
+        {
+            lexers ~= new Lexer(source);
+            lexers[$-1].lex();
+        }
+        catch (FileException fe)
+        {
+            stderr.writeln("Exception raised when lexing ", source);
+            stderr.writeln(fe.msg);
+            return 1;
+        }
+    }
+    if (options.mtime)
+    {
+        timer.peek();
     }
     if (options.until == Until.lexing)
     {
+        if (options.mtime)
+            timer.print();
         writeln("lexing phase finished, exited.");
         return 0;
     }
@@ -113,8 +174,14 @@ int main(string[] args)
         parsers ~= new Parser(lexer);
         if (UnitContainerAstNode uc = parsers[$-1].parse())
         {
-            //DebugVisitor dbgv = new DebugVisitor(uc);
-            //dbgv.printText;
+            if (options.print)
+            {
+                writeln("AST for ", lexer.filename);
+                DebugVisitor dbgv = new DebugVisitor();
+                dbgv.visit(uc);
+                dbgv.printText;
+                writeln;
+            }
         }
         else
         {
@@ -122,8 +189,14 @@ int main(string[] args)
             return 1;
         }
     }
+    if (options.mtime)
+    {
+        timer.peek();
+    }
     if (options.until == Until.parsing)
     {
+        if (options.mtime)
+            timer.print();
         writeln("parsing phase finished, exited.");
         return 0;
     }
@@ -135,11 +208,22 @@ int main(string[] args)
         if (!unitSemantic(parser.unitContainer, lexers[i]))
             return 1;
     }
-
+    if (options.mtime)
+    {
+        timer.peek();
+    }
     if (options.until == Until.semantic)
     {
+        if (options.mtime)
+            timer.print();
         writeln("semantic phase finished, exited.");
         return 0;
+    }
+
+    if (options.mtime)
+    {
+        timer.peek();
+        timer.print();
     }
 
     return 0;
