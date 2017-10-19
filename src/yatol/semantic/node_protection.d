58e889eaaac1ac3e34f3ab59f35c48a9b43c7f8f
Basile Burg <basile.b@gmx.com>
2017-10-19 06:52:21 +0200

    test and cov node protection visitor

--- src/yatol/semantic/node_protection.d
+++ src/yatol/semantic/node_protection.d
@@ -6,7 +6,7 @@ import
     yatol.token, yatol.lexer, yatol.ast;
 
 /**
- * An AST visitor that set the protection of each node.
+ * An AST visitor that sets the protection of each node.
  */
 final class NodeProtectionVisitor: AstVisitor
 {
@@ -73,23 +73,18 @@ public:
     /// Returns: $(D true) if the protections have been set correctly.
     bool success() {return _success;}
 
-    override void visit(DeclarationAstNode node)
-    {
-        node.accept(this);
-    }
-
     override void visit(ProtectionDeclarationAstNode node)
     {
         with (Protection) switch (node.protection.text())
         {
             case "public":
-                pushProtection(public_);
+                overwriteProtection(public_);
                 break;
             case "private":
-                pushProtection(private_);
+                overwriteProtection(private_);
                 break;
             case "protected":
-                pushProtection(protected_);
+                overwriteProtection(protected_);
                 break;
             default:
                 writefln("%s(%d,%d): error, `%s` is not a valid protection, " ~
@@ -108,6 +103,14 @@ public:
         popProtection;
     }
 
+    override void visit(InterfaceDeclarationAstNode node)
+    {
+        setFields(node);
+        pushProtection(Protection.public_);
+        node.accept(this);
+        popProtection;
+    }
+
     override void visit(StructDeclarationAstNode node)
     {
         setFields(node);
@@ -116,9 +119,126 @@ public:
         popProtection;
     }
 
-    override void visit(ImportDeclarationAstNode node)
+    override void visit(UnionDeclarationAstNode node)
     {
         setFields(node);
+        pushProtection(Protection.public_);
+        node.accept(this);
+        popProtection;
     }
+
+    override void visit(EnumDeclarationAstNode node)
+    {
+        setFields(node);
+        node.accept(this);
+    }
+
+    override void visit(AkaDeclarationAstNode node)
+    {
+        setFields(node);
+        node.accept(this);
+    }
+
+    override void visit(ImportDeclarationAstNode node)
+    {
+        node.accept(this);
+        setFields(node);
+    }
+
+    override void visit(VariableDeclarationItemAstNode node)
+    {
+        node.accept(this);
+        setFields(node);
+    }
+
+    override void visit(FunctionDeclarationAstNode node)
+    {
+        node.accept(this);
+        setFields(node);
+    }
+}
+
+unittest
+{
+    class ProtectionChecker: AstVisitor
+    {
+        alias visit = AstVisitor.visit;
+
+        void check(N)(N node)
+        {
+            import std.algorithm;
+            if (node.name.text.startsWith("public"))
+                assert(node.isPublic, node.name.text);
+            else if (node.name.text.startsWith("private"))
+                assert(node.isPrivate);
+            else if (node.name.text.startsWith("protected"))
+                assert(node.isProtected);
+        }
+
+        override void visit(VariableDeclarationItemAstNode node)
+        {
+            node.accept(this);
+            check(node);
+        }
+    }
+
+    enum line = __LINE__ + 1;
+    enum string source = q{
+        unit a;
+
+        const s8 public1 = 8;
+        const s8 public2 = 16;
+
+        struct Foo
+        {
+        protection(private)
+
+            var s8 private_member_1, private_member_2;
+            is b aka a;
+
+            interface Bar{}
+
+        protection(public)
+
+            var s8 public_member_1, public_member_2;
+
+            union U{}
+        }
+
+        class Bar
+        {
+        protection(private)
+
+            var s8 private_member_1, private_member_2;
+
+        protection(protected)
+
+            var s8 protected_member_1, protected_member_2;
+
+            import a.b;
+
+            enum E {e}
+
+        protection(public)
+
+            function public1();
+        }
+
+        protection(invalid)
+
+            function public1();
+    };
+
+    import yatol.lexer, yatol.parser;
+
+    Lexer lx;
+    lx.setSourceFromText(source, __FILE_FULL_PATH__, line);
+    lx.lex();
+    UnitContainerAstNode uc = Parser(&lx).parse();
+    NodeProtectionVisitor np = new NodeProtectionVisitor(uc, &lx);
+    np.visit(uc);
+    ProtectionChecker pc = new ProtectionChecker;
+    pc.visit(uc);
+    assert(!np.success);
 }
 
