939a3a06378f7f7d9b1633f8830283f1b8f4b18f
Basile Burg <basile.b@gmx.com>
2018-05-10 14:16:33 +0200

    try to fix CI

--- test.sh
+++ test.sh
@@ -13,18 +13,15 @@ cd grammar && dub run --config=generate && dub run --config=test
 cd ..
 
 # in-source unit tests + cover main appl piping features
-if [ -f bin/yatol ]; then rm bin/yatol; fi
 dub build --build=unittest-cov --config=compiler --compiler=${DC}
 echo "unit b;" > b.ya && bin/yatol -v -p -a -t b.ya <<< "unit a;"
-rm bin/yatol
 
 # release build
-dub --build=release --config=compiler --compiler=${DC}
-rm bin/yatol
-dub build --build=release --config=library --compiler=${DC}
+dub build --build=release --config=compiler --force --compiler=${DC}
+dub build --build=release --config=library --force --compiler=${DC}
 
 # file-based tests
-dub --build=cov --config=compiler --compiler=${DC}
+dub build --build=cov --config=compiler --force --compiler=${DC}
 cd tests
 $DC test.d
 ./test
